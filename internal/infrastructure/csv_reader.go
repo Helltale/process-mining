44f50418149bcb7f0626f4d082a7ae796f725ed0
Dmitry Andreev <andreev.dp@bk.ru>
2025-03-18 11:41:32 +0400

    feat: add ReadAndProcessConcurrent

--- internal/infrastructure/csv_reader.go
+++ internal/infrastructure/csv_reader.go
@@ -4,7 +4,9 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"runtime"
 	"strings"
+	"sync"
 )
 
 type CSVReader struct{}
@@ -38,6 +40,69 @@ func (r *CSVReader) ReadAndProcess(filePath string, processFunc func([]string) e
 	}
 
 	if err := scanner.Err(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// работает даже дольше
+func (r *CSVReader) ReadAndProcessConcurrent(filePath string, processFunc func([]string) error) error {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	// Пропускаем заголовок
+	if scanner.Scan() {
+		header := scanner.Text()
+		log.Printf("Пропущен заголовок: %s", header)
+	}
+
+	// Создаем канал для передачи строк
+	lines := make(chan string, 100) // Буферизованный канал для строк
+	errChan := make(chan error, 1)  // Канал для ошибок
+
+	// Горутина для чтения строк из файла
+	go func() {
+		defer close(lines)
+		for scanner.Scan() {
+			lines <- scanner.Text()
+		}
+		if err := scanner.Err(); err != nil {
+			errChan <- err
+		}
+	}()
+
+	// Горутины для обработки строк
+	numWorkers := runtime.NumCPU() // Количество ядер процессора
+	var wg sync.WaitGroup
+	wg.Add(numWorkers)
+
+	for i := 0; i < numWorkers; i++ {
+		go func() {
+			defer wg.Done()
+			for line := range lines {
+				record := strings.Split(line, ",")
+				if err := processFunc(record); err != nil {
+					errChan <- err
+					return
+				}
+			}
+		}()
+	}
+
+	// Ожидаем завершения всех горутин
+	go func() {
+		wg.Wait()
+		close(errChan)
+	}()
+
+	// Проверяем наличие ошибок
+	if err := <-errChan; err != nil {
 		return err
 	}
 
